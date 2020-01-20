f55ba64da562eacf118e488d7bae086789d6d2fd
github username <github email address>
2020-01-20 15:10:15 -0500

    added pagination and sort for tasks

--- src/controllers/tasks_controller.js
+++ src/controllers/tasks_controller.js
@@ -18,8 +18,13 @@ router.post('/tasks', auth, async (req, res) => {
 router.get('/tasks', auth, async (req, res) => {
     //req.query.completed will give you any query forms
     const match = {}
+    const sort = {}
     if (req.query.completed) {
         match.completed = req.query.completed === 'true'
+    }
+    if (req.query.sort) {
+        const parts = req.query.sortBy.split(":")
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
     }
     try {
         await req.user.populate({
@@ -27,7 +32,8 @@ router.get('/tasks', auth, async (req, res) => {
             match,
             options: {
                 limit: parseInt(req.query.limit),
-                skip: parseInt(req.query.skip)
+                skip: parseInt(req.query.skip),
+                sort
             }
         }).execPopulate()
         res.send(req.user.tasks)
